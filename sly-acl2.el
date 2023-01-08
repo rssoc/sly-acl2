ad883acda71ed1e364e0a1e2d5f952e50bd1bf3e
rssoc <8051656+rssoc@users.noreply.github.com>
2023-01-07 21:08:58 -0500

    Registered package with sly.

---
+++ sly-acl2.el
@@ -0,0 +1,11 @@
+;;; sly-acl2.el -- ACL2 support for sly.
+;;;
+(require 'sly)
+
+
+(define-sly-contrib sly-acl2
+  "ACL2 support for sly."
+  (:slynk-dependencies slynk-acl2))
+
+
+(provide 'sly-acl2)
